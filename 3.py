c6ad77b5a8fae0e5f881dc7ea0a5289b1d6d5b05
alisa <alisa.solodovnik@gmail.com>
2024-03-22 23:17:00 +0700

    update task 3

--- 3.py
+++ 3.py
@@ -3,7 +3,7 @@ dig1 = num // 1000
 dig2 = (num // 100) % 10
 dig3 = (num % 100) // 10
 dig4 = num % 10
-if dig1 == dig4 and di2 == dig3:
+if dig1 == dig4 and dig2 == dig3:
     print('Настоящее')
 else:
     print('Кривое')
