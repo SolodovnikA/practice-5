03bf7cd3fad5722c2e002532afdb34e0f3e6df3e
alisa <alisa.solodovnik@gmail.com>
2024-03-23 09:13:48 +0700

    update task 2

--- 2.py
+++ 2.py
@@ -1,10 +1,13 @@
-import turtle as t
-
 xc = int(input())
 yc = int(input())
 r = int(input())
 x = int(input())
 y = int(input())
+
+a = 'Точка внутри окружности'
+b = 'Точка вне окружности'
+
+import turtle as t
 
 t.pu()
 t.goto(xc, yc)
@@ -14,10 +17,14 @@ t.circle(r)
 t.pu()
 t.goto(x, y)
 t.pd()
-t.dot(3)
+t.dot(10)
 
-d = ((x-xc)**2 + (y-yc)**2)**0.5
+t.pu()
+t.goto(x-20, y-20)
+t.pd()
+
+d = ((x-xc)2 + (y-yc)2)**0.5
 if d < r:
-    t.write('Точка внутри окружности')
+    t.write(a)
 elif d > r:
-    t.write('Точка вне окружности')
+    t.write(b)
